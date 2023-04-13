1e9e080fde7bc89865623eac6091b3f90643c28c
richi <richidhimar45@gmail.com>
2023-04-13 12:05:39 +0530

    update the permission

--- api/permissions.py
+++ api/permissions.py
@@ -11,17 +11,21 @@ class IsAdminOrReadOnly(BasePermission):
         return request.user and request.user.is_staff
     
 class IsAuthenticated(BasePermission):
-    def has_add_permission(self, request, obj=None):
-        if request.user == Task.user and Permission.can_create:
+    def has_permission(self, request, view):
+        if request.user in SAFE_METHODS:
             return True
         return request.user and request.user.is_active
     
-    def has_delete_permission(self, request, obj=None):
-        if request.user == Task.user and Permission.can_delete:
-            return True
-        return request.user and request.user.is_active
+    # def has_permission(self, request, view):
+    #     if request.user == Task.user:
+    #         print("in if task")
+    #         if Permission.can_delete == True:
+    #             print("in if per")
+    #             return True
+    #     return request.user and request.user.is_active
     
-    def has_change_permission(self, request, obj=None):
-        if request.user == Task.user:
-            return True
-        return request.user and request.user.is_active
+    # def has_permission(self, request, view):
+    #     if request.user == Task.user:
+    #         Permission.can_edit == True
+    #         return True
+    #     return request.user and request.user.is_active
